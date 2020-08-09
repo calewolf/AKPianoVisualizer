35fd280efbd2883ddc8aafeb82da38272db28d41
caleqw2 <caleqw2@illinois.edu>
2020-08-09 14:19:18 -0500

    Responds to MIDI input with note number display

--- AKPianoVisualizer/ContentView.swift
+++ AKPianoVisualizer/ContentView.swift
@@ -1,23 +1,43 @@
 //
 //  ContentView.swift
-//  AKPianoVisualizer
+//  SampleAKProject
 //
-//  Created by Cale Wolf on 8/9/20.
+//  Created by Cale Wolf on 8/8/20.
 //  Copyright © 2020 Cale Wolf. All rights reserved.
 //
 
 import SwiftUI
+import AudioKit
 
 struct ContentView: View {
+    var midi = AudioKit.midi
+    @State var midiNum: String = "Num"
+    
     var body: some View {
-        Text("Committing after pull request")
+        Text(midiNum)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onAppear {
+                self.midi.openInput()
+                self.midi.addListener(Listener(midiNum: self.$midiNum))
+            }
     }
 }
 
+struct Listener: AKMIDIListener {
+    @Binding var midiNum: String
+    
+    func receivedMIDINoteOn(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, channel: MIDIChannel,
+                            portID: MIDIUniqueID? = nil, offset: MIDITimeStamp = 0) {
+        print(noteNumber)
+        midiNum = noteNumber.description
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
