82e4865f4e89acc779b718413afade65f2f05c6f
caleqw2 <caleqw2@illinois.edu>
2020-08-09 12:04:45 -0500

    Initial Commit

---
+++ AKPianoVisualizer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AKPianoVisualizer
+//
+//  Created by Cale Wolf on 8/9/20.
+//  Copyright © 2020 Cale Wolf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
